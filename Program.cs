024ff5bba3c22cda8da8473dd78e52d6c622f630
Tyler Colon <colont@usf.edu>
2022-02-09 10:36:04 -0500

    Finally fixed incrment value issues

--- Program.cs
+++ Program.cs
@@ -22,29 +22,31 @@ namespace TechAssignment3_UsingITerativeStatements
                 // This variable will be used to perform the iterative statement and is parsed as an integer
                 int CADvalue = int.Parse(input);
                 double USDollars;
+
                 //converting CAD value to US dOllars
-                USDollars = (CADvalue*.0792367);
+                USDollars = (CADvalue * .0792367);
                 //Using if statement to set paramters for values between 0 and 200
-                if ((CADvalue >= 0) && (CADvalue <= 200))
+                if ((CADvalue > 0) && (CADvalue <= 200))
                     {
                     //Executing a for loop to compute/ print a table of values for the manager
                     Console.WriteLine("CAD increment value:" + CADvalue);
-
-                    for (int i = 0; i < CADvalue; ++i)
+                   
+                    //for loop iterative statement to produce CADvalues
+                    for (int i = 0; i < CADvalue; i++ )
                         {
                         // It is statement showing the values of the CAD and USDOLLARS
-                        Console.WriteLine("CAD:  " + CADvalue.ToString() + "    USDollars: " + USDollars.ToString());
+                        Console.WriteLine("CAD:  " + i + "    USDollars: " + i*USDollars);
                         }
 
-                    //if there is an input error teling the user to try again
-                    Console.WriteLine("Please enter an integer value and run it again");
+                    //if there is an input error telling the user to try again
+                    Console.WriteLine("If error, make sure you are entering a integer value between 0 and 200");
                     Console.WriteLine("Press any key to exit the program");
                     Console.ReadKey(true);
                 }
-
             }
             catch
             {
+                //After the catch if errors occur
                 Console.WriteLine("Please enter an integer value and run it again");
                 Console.WriteLine("Press any key to exit the program");
                 Console.ReadKey(true);
